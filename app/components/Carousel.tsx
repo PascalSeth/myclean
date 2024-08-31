607ed86b9e5be04ac42571de7b23c5ae670e2220
tranzbook <tranzbook1@gmail.com>
2024-08-31 15:38:46 +0000

    addition

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -12,7 +12,9 @@ interface SlideProps {
 const slides: SlideProps[] = [
   { imgSrc: 'https://images.unsplash.com/photo-1650112274147-03a2dba421c8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', altText: 'Slide 1' },
   { imgSrc: 'https://images.unsplash.com/photo-1510251197878-a2e6d2cb590c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', altText: 'Slide 2' },
-  { imgSrc: 'https://plus.unsplash.com/premium_photo-1663076452996-abef3ccfc4f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2FzdGUlMjBtYW5hZ2VtZW50fGVufDB8fDB8fHww', altText: 'Slide 3' },
+  { imgSrc: 'https://images.unsplash.com/photo-1594741243587-90ef02c1dc68?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', altText: 'Slide 3' },
+  { imgSrc: 'https://www.graphic.com.gh/images/2021/oct/26/aboboyaa3.jpg', altText: 'Slide 3' },
+
 ];
 
 const Carousel: React.FC = () => {
