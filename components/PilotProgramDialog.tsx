02e61cdd39dbee885cf428b630d729887026cc8b
pascalseth <pascalelikem@gmail.com>
2025-09-24 17:52:38 +0000

    new

--- components/PilotProgramDialog.tsx
+++ components/PilotProgramDialog.tsx
@@ -39,6 +39,7 @@ const PilotProgramDialog: React.FC<PilotProgramDialogProps> = ({ open: controlle
       <DialogPrimitive.Portal>
         <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
         <DialogPrimitive.Content className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border-0 bg-transparent p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] mx-4">
+          <DialogPrimitive.Title className="sr-only">Pilot Program - We're Live in Pokuase!</DialogPrimitive.Title>
           <motion.div
             initial={{ scale: 0.9, opacity: 0 }}
             animate={{ scale: 1, opacity: 1 }}
